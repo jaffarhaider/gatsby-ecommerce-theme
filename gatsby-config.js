47bd6224a791d73e6c1e48c224852c5eb09e28da
jaffar <jaffarhaider96@gmail.com>
2023-09-02 17:36:57 +0500

    title change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Sydney Ecommerce Theme`,
+    title: `Hoopoes`,
     siteUrl: `https://jamm.matter.design`,
   },
   plugins: [
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Sydney Ecommerce Theme`,
-        short_name: `Sydney`,
+        name: `Hoopoes`,
+        short_name: `Hoopoess`,
         start_url: `/`,
         background_color: `#000000`,
         theme_color: `#ffffff`,
